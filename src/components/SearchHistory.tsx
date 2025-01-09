14e1b8602b421262916a55abd236e17921125ead
Nico786 <nicolas.theye@gmail.com>
2025-01-09 21:01:05 +0100

    update component type

--- src/components/SearchHistory.tsx
+++ src/components/SearchHistory.tsx
@@ -1,22 +1,24 @@
-import { Pokemon } from "@/lib/types";
 import React from "react";
+import { Pokemon } from "@/lib/types";
 import { Col, Row } from "react-bootstrap";
 
 interface SearchHistoryProps {
-  pokemons: { name: string; sprite: string }[];
+  pokemons: Pokemon[];
 }
 
 export const SearchHistory: React.FC<SearchHistoryProps> = ({ pokemons }) => {
-  //console log les pokemons
-  console.log(pokemons);
   return (
     <div>
       <h2>Historique</h2>
       <Row className="search-history">
         {pokemons.map((pokemon, index) => (
           <Col xs={4} md={3} lg={1} key={index} className="pokemon-card">
-            <img src={pokemon.sprite} alt={pokemon.name} width={150} />
-            <span>{pokemon.name}</span>
+            <img
+              src={pokemon.sprites.regular}
+              alt={pokemon.name.fr}
+              width={150}
+            />
+            <span>{pokemon.name.fr}</span>
           </Col>
         ))}
       </Row>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,22 @@
 import { useLazyQuery } from "@apollo/client";
 import { useState } from "react";
-import InputField from "@/components/InputField";
 import { GET_POKEMON } from "@/graphql/queries";
+import { Pokemon } from "@/lib/types";
+import InputField from "@/components/InputField";
 import CardDetails from "@/components/CardDetails";
 import SearchHistory from "@/components/SearchHistory";
 
 const Home = () => {
   const [getPokemon, { data, loading, error }] = useLazyQuery(GET_POKEMON);
-  const [modalShow, setModalShow] = useState(false);
-  const [searchHistory, setSearchHistory] = useState<
-    { name: string; sprite: string }[]
-  >([]);
+  const [modalShow, setModalShow] = useState<boolean>(false);
+  const [searchHistory, setSearchHistory] = useState<Pokemon[]>([]);
 
   const handleSearch = (name: string) => {
     getPokemon({ variables: { name } });
     if (data && data.pokemon) {
-      const pokemon = data.pokemon;
+      const pokemon: Pokemon = data.pokemon;
       setModalShow(true);
-      setSearchHistory((prevSearch) => [
-        ...prevSearch,
-        { name: pokemon.name.fr, sprite: pokemon.sprites.regular },
-      ]);
+      setSearchHistory([...searchHistory, pokemon]);
     }
   };
 
@@ -36,7 +32,7 @@ const Home = () => {
             onHide={() => setModalShow(false)}
             pokemon={data.pokemon}
           />
-          <SearchHistory pokemons={searchHistory} />
+          <SearchHistory pokemons={searchHistory as Pokemon[]} />
         </div>
       )}
     </div>
