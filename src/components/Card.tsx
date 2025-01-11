069e6a7fd594d70867771f603dfdbac69b6c9ae5
Nico786 <nicolas.theye@gmail.com>
2025-01-11 16:23:23 +0100

    flip cards by defaut, reveal them on click

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import { Pokemon } from "@/lib/types";
-import React from "react";
+import React, { useState } from "react";
+import pokeball from "@/assets/images/pokeball.png";
 
 type CardProps = {
   pokemon: Pokemon;
@@ -7,17 +8,26 @@ type CardProps = {
 };
 
 const Card: React.FC<CardProps> = ({ pokemon, onClick }) => {
+  const [isRevealed, setIsRevealed] = useState<boolean>(false);
+
   const handleClick = () => {
+    setIsRevealed(true);
     if (onClick) {
       onClick();
     }
   };
 
+  const src = isRevealed ? pokemon.sprites.regular : pokeball;
+  const name = isRevealed ? pokemon.name.fr : "?";
+
   return (
     <div>
-      <div className="pokemon-card" onClick={handleClick}>
+      <div
+        className={`pokemon-card ${isRevealed ? "card-revealed" : ""}`}
+        onClick={handleClick}
+      >
         <img
-          src={pokemon.sprites.regular}
+          src={src}
           alt={pokemon.name.fr}
           width={100}
           className="img-fluid"
@@ -25,7 +35,7 @@ const Card: React.FC<CardProps> = ({ pokemon, onClick }) => {
         <p>
           n°{pokemon.pokedex_id}
           <br />
-          {pokemon.name.fr}
+          {name}
         </p>
       </div>
     </div>
